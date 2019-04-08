5af01553a65ad6143d0659262cca98ca6c306dde
dislersd <dislersd@gmail.com>
2019-04-08 14:33:14 -0700

    built tests for succeed method

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -3,7 +3,6 @@ const enhancer = require("./enhancer");
 describe("actions", () => {
   //
   describe("repair", () => {
-    //
     it("return item durabilty at 100", () => {
       expect(enhancer.repair(
       {
@@ -17,13 +16,39 @@ describe("actions", () => {
         enhancement: 0,
         durability: 100
       }
-      );
-
+      )
     });
   });
-  // describe("succeed", () => {
-  //   succeed(spear);
-  //   const num = spear.enhancement;
-  //   expect(spear.enhancement).toBe(num)
-  // })
+  describe("succeed", () => {
+    it("return item enhancement + 1", () => {
+      expect(enhancer.succeed(
+        {
+          name: 'spear',
+          enhancement: 0,
+          durability: 100
+        }
+      )).toEqual(
+        {
+          name: 'spear',
+          enhancement: 1,
+          durability: 100
+        }
+      )
+    })
+    it("item is at enhancement lvl 20", () => {
+      expect(enhancer.succeed(
+        {
+          name: 'spear',
+          enhancement: 20,
+          durability: 100
+        }
+      )).toEqual(
+        {
+          name: 'spear',
+          enhancement: 20,
+          durability: 100
+        }
+      )
+    })
+  })
 });
